bac373ac5d035eff7e0952e118309c5057ccf1c2
Robert M. Parker <rparker@yamiko.ninja>
2016-05-22 09:56:18 -0700

    live updates

--- wp-content/themes/sayenkodesign/index.php
+++ wp-content/themes/sayenkodesign/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
 
+wp_title();exit;
+
 $twig = $container->get("twig.environment");
 echo $twig->render('base.html.twig', [
     'dropdown_menu_walker' => new \App\WordPress\DropDownMenuWalker(),
